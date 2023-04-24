3d4b62a53bad1145e3cdb165e07e1953f079c142
mvazgon <absimiliard@hotmail.com>
2023-04-24 22:48:07 +0200

    modificado archivo api.py

--- app/api.py
+++ app/api.py
@@ -25,7 +25,7 @@ def add(op_1, op_2):
 
 
 @api_application.route("/calc/substract/<op_1>/<op_2>", methods=["GET"])
-def substract(op_1, op_2):
+def substract(op_1, op_2):m
     try:
         num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
         return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
@@ -35,7 +35,7 @@ def substract(op_1, op_2):
 @api_application.route("/calc/sqrt/<op_1>", methods=["GET"])
 def sqrt(op_1):
     try:
-        num_1=util.convert_to_number(op_1)
-        return ("{}".format(CALCULATOR.sqrt(num_1)), http.client.OK, HEADERS)
+        num_1, num_2 = util.convert_to_number(op_1), util.convert_to_number(op_2)
+        return ("{}".format(CALCULATOR.sqrt(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
