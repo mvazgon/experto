6f45a363e59e6ce5da5ba57402ab374cb60d984a
mvazgon <absimiliard@hotmail.com>
2023-04-24 22:27:20 +0200

    add endpoint sqrt

--- app/api.py
+++ app/api.py
@@ -31,3 +31,11 @@ def substract(op_1, op_2):
         return ("{}".format(CALCULATOR.substract(num_1, num_2)), http.client.OK, HEADERS)
     except TypeError as e:
         return (str(e), http.client.BAD_REQUEST, HEADERS)
+
+@api_application.route("/calc/sqrt/<op_1>", methods=["GET"])
+def sqrt(op_1):
+    try:
+        num_1 util.convert_to_number(op_1)
+        return ("{}".format(CALCULATOR.sqrt(num_1)), http.client.OK, HEADERS)
+    except TypeError as e:
+        return (str(e), http.client.BAD_REQUEST, HEADERS)
